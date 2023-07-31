05b5dcca60d587578626b32b5612282dea6cc2f7
pck-llight <23sunrin113@sunrint.hs.kr>
2023-07-31 10:58:45 +0900

    비밀번호 찾기 화면 XML

---
+++ app/src/main/java/com/example/ploggers/forgot_pw.kt
@@ -0,0 +1,11 @@
+package com.example.ploggers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class forgot_pw : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_pw)
+    }
+}
