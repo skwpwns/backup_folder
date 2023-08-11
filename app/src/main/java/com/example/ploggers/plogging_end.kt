03f15a05251ecc49a02e0df121f3f172b27802bf
JH612 <jaehee1717612@gmail.com>
2023-08-11 11:30:50 +0900

    XML activity file 추가

---
+++ app/src/main/java/com/example/ploggers/plogging_end.kt
@@ -0,0 +1,11 @@
+package com.example.ploggers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class plogging_end : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plogging_end)
+    }
+}
